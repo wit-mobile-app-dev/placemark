9a72e3425cae34eae7226a72c7435cc54073ef3a
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-20 09:42:38 +0100

    moce placemarks list to MainApp

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,8 +3,11 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.placemark.models.PlacemarkModel
 
 class MainApp : Application(), AnkoLogger {
+
+  val placemarks = ArrayList<PlacemarkModel>()
 
   override fun onCreate() {
     super.onCreate()
