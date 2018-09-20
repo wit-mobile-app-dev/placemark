71ea5f8309cac45170b5a41ad57ec88d4b54ed50
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-20 10:06:16 +0100

    include PlacemarkAdapter in PlacemarkListActivity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -2,8 +2,16 @@ package org.wit.placemark.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_placemark_list.*
+import kotlinx.android.synthetic.main.card_placemark.view.*
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkListActivity : AppCompatActivity() {
 
@@ -13,6 +21,31 @@ class PlacemarkListActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_list)
     app = application as MainApp
+
+    val layoutManager = LinearLayoutManager(this)
+    recyclerView.layoutManager = layoutManager
+    recyclerView.adapter = PlacemarkAdapter(app.placemarks)
   }
 }
 
+class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) : RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+    return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_placemark, parent, false))
+  }
+
+  override fun onBindViewHolder(holder: MainHolder, position: Int) {
+    val placemark = placemarks[holder.adapterPosition]
+    holder.bind(placemark)
+  }
+
+  override fun getItemCount(): Int = placemarks.size
+
+  class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(placemark: PlacemarkModel) {
+      itemView.placemarkTitle.text = placemark.title
+      itemView.description.text = placemark.description
+    }
+  }
+}
