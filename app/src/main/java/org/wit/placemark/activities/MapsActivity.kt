20a0ce1d3936b5807c2f38b43006f850144fcf79
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-27 07:45:30 +0100

    recover location from MapsActivity

--- app/src/main/java/org/wit/placemark/activities/MapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/MapsActivity.kt
@@ -1,5 +1,7 @@
 package org.wit.placemark.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,11 +10,12 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
 import org.wit.placemark.models.Location
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
   private lateinit var map: GoogleMap
   var location = Location()
@@ -28,6 +31,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
   override fun onMapReady(googleMap: GoogleMap) {
     map = googleMap
+    map.setOnMarkerDragListener(this)
     val loc = LatLng(location.lat, location.lng)
     val options = MarkerOptions()
         .title("Placemark")
@@ -36,5 +40,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         .position(loc)
     map.addMarker(options)
     map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+  }
+
+  override fun onMarkerDragStart(marker: Marker) {
+  }
+
+  override fun onMarkerDrag(marker: Marker) {
+  }
+
+  override fun onMarkerDragEnd(marker: Marker) {
+    location.lat = marker.position.latitude
+    location.lng = marker.position.longitude
+    location.zoom = map.cameraPosition.zoom
+  }
+
+  override fun onBackPressed() {
+    val resultIntent = Intent()
+    resultIntent.putExtra("location", location)
+    setResult(Activity.RESULT_OK, resultIntent)
+    finish()
+    super.onBackPressed()
   }
 }
