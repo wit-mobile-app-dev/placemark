d4751f3fc96740eb2e62cf6f2290b4226e32fa46
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-27 07:40:49 +0100

    send location to MapsActivity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -23,6 +23,8 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   var placemark = PlacemarkModel()
   lateinit var app: MainApp
   val IMAGE_REQUEST = 1
+  val LOCATION_REQUEST = 2
+  var location = Location(52.245696, -7.139102, 15f)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -68,8 +70,7 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     }
 
     placemarkLocation.setOnClickListener {
-      val location = Location(52.245696, -7.139102, 15f)
-      startActivity (intentFor<MapsActivity>().putExtra("location", location))
+      startActivityForResult(intentFor<MapsActivity>().putExtra("location", location), LOCATION_REQUEST)
     }
   }
 
@@ -95,6 +96,11 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
           placemark.image = data.getData().toString()
           placemarkImage.setImageBitmap(readImage(this, resultCode, data))
           chooseImage.setText(R.string.change_placemark_image)
+        }
+      }
+      LOCATION_REQUEST -> {
+        if (data != null) {
+          location = data.extras.getParcelable<Location>("location")
         }
       }
     }
