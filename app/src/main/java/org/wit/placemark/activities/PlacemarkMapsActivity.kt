45af6bf748afe4d18d7ef8a2eca188e760141577
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-27 13:17:12 +0100

    introduce appBar and simplify activity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,10 +1,8 @@
 package org.wit.placemark.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import org.wit.placemark.R
-
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
 
 class PlacemarkMapsActivity : AppCompatActivity() {
@@ -12,12 +10,6 @@ class PlacemarkMapsActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_maps)
-    setSupportActionBar(toolbar)
-
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
-    }
+    setSupportActionBar(toolbarMaps)
   }
-
 }
