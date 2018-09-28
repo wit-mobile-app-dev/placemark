6882f2d44df6078762b8c94e8cef45d979c71614
Eamonn de Leastar <edeleastar@wit.ie>
2018-09-28 07:30:01 +0100

    initialize map object

--- app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -2,14 +2,19 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.placemark.R
 import kotlinx.android.synthetic.main.activity_placemark_maps.*
+import kotlinx.android.synthetic.main.content_placemark_maps.*
 
 class PlacemarkMapsActivity : AppCompatActivity() {
+
+  lateinit var map: GoogleMap
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark_maps)
     setSupportActionBar(toolbarMaps)
+    mapView.onCreate(savedInstanceState);
   }
 }
